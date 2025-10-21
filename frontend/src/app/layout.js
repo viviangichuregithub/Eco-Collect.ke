89b8d8341fbe231b1e22e5d1ee2625fddd4376a5
viviangichuregithub <128038022+viviangichuregithub@users.noreply.github.com>
2025-10-21 15:49:56 +0300

    Fix title formatting and add styles to body
    Updated layout.js to fix title formatting and add body styles.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,18 +2,27 @@ import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], 
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata = {
-  title: "Eco-Collect,Kenya",
+  title: "Eco-Collect, Kenya",
   description: "Connecting citizens and corporations for a circular economy",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body
+        className={poppins.className}
+        style={{
+          margin: 0,
+          overflow: "hidden",
+          height: "100vh",
+        }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
