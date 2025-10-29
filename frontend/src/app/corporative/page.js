5b26e551906093453963b05e3369df2381c15b5e
Vivian Gichure <viviangichure@gmail.com>
2025-10-29 16:32:37 +0300

    renamed reset password page

--- frontend/src/app/corporative/page.js
+++ frontend/src/app/corporative/page.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Logo from "../Logo.svg";
-import CorporativeNavBar from "@/components/CorporativeNavBar";
+import CorporativeNavBar from "../../components/CorporativeNavBar";
 import { getCurrentUser, logout } from "../../lib/user";
 
 export default function CorporativePage() {
